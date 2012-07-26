69f453566d17054c61516d3a4fd9662b9bbe7d50
sihorton <sihorton@github.com>
2012-07-27 00:18:56 +0200

    compiled new gaia profile 2012-07-27

--- user.js
+++ user.js
@@ -1,19 +1,23 @@
-user_pref("browser.homescreenURL","http://system.gaiamobile.org");
-user_pref("browser.manifestURL","http://system.gaiamobile.org/manifest.webapp");
+user_pref("browser.manifestURL","app://system.gaiamobile.org/manifest.webapp");
 
+user_pref("browser.homescreenURL","app://system.gaiamobile.org/index.html");
 user_pref("dom.allow_scripts_to_close_windows", true);
 
-user_pref("b2g.privileged.domains", "http://fm.gaiamobile.org,http://gallery.gaiamobile.org,http://contacts.gaiamobile.org,http://clock.gaiamobile.org,http://homescreen.gaiamobile.org,http://calendar.gaiamobile.org,http://browser.gaiamobile.org,http://keyboard.gaiamobile.org,http://email.gaiamobile.org,http://music.gaiamobile.org,http://video.gaiamobile.org,http://sms.gaiamobile.org,http://system.gaiamobile.org,http://camera.gaiamobile.org,http://calculator.gaiamobile.org,http://dialer.gaiamobile.org,http://settings.gaiamobile.org,http://template.gaiamobile.org,http://uitest.gaiamobile.org,http://test-agent.gaiamobile.org,http://penguinpop.gaiamobile.org,http://crystalskull.gaiamobile.org,http://cuttherope.gaiamobile.org,http://towerjelly.gaiamobile.org,http://tasks.gaiamobile.org,http://cubevid.gaiamobile.org");
+user_pref("dom.send_after_paint_to_content", true);
+
+user_pref("b2g.privileged.domains", "app://fm.gaiamobile.org,app://gallery.gaiamobile.org,app://contacts.gaiamobile.org,app://clock.gaiamobile.org,app://homescreen.gaiamobile.org,app://calendar.gaiamobile.org,app://browser.gaiamobile.org,app://keyboard.gaiamobile.org,app://email.gaiamobile.org,app://music.gaiamobile.org,app://video.gaiamobile.org,app://sms.gaiamobile.org,app://system.gaiamobile.org,app://camera.gaiamobile.org,app://calculator.gaiamobile.org,app://dialer.gaiamobile.org,app://settings.gaiamobile.org,app://template.gaiamobile.org,app://uitest.gaiamobile.org,app://test-agent.gaiamobile.org,app://penguinpop.gaiamobile.org,app://crystalskull.gaiamobile.org,app://cuttherope.gaiamobile.org,app://towerjelly.gaiamobile.org,app://tasks.gaiamobile.org,app://cubevid.gaiamobile.org");
+
+user_pref("network.http.max-connections-per-server", 15);
 
 user_pref("network.dns.localDomains", "gaiamobile.org,fm.gaiamobile.org,gallery.gaiamobile.org,contacts.gaiamobile.org,clock.gaiamobile.org,homescreen.gaiamobile.org,calendar.gaiamobile.org,browser.gaiamobile.org,keyboard.gaiamobile.org,email.gaiamobile.org,music.gaiamobile.org,video.gaiamobile.org,sms.gaiamobile.org,system.gaiamobile.org,camera.gaiamobile.org,calculator.gaiamobile.org,dialer.gaiamobile.org,settings.gaiamobile.org,template.gaiamobile.org,uitest.gaiamobile.org,test-agent.gaiamobile.org,penguinpop.gaiamobile.org,crystalskull.gaiamobile.org,cuttherope.gaiamobile.org,towerjelly.gaiamobile.org,tasks.gaiamobile.org,cubevid.gaiamobile.org");
-user_pref("dom.power.whitelist","http://system.gaiamobile.org,http://dialer.gaiamobile.org,http://dialer.gaiamobile.org/oncall.html,http://test-agent.gaiamobile.org");
-user_pref("dom.sms.whitelist","http://sms.gaiamobile.org");
-user_pref("dom.mozContacts.whitelist","http://contacts.gaiamobile.org,http://sms.gaiamobile.org,http://dialer.gaiamobile.org,http://dialer.gaiamobile.org/oncall.html");
-user_pref("dom.telephony.app.phone.url","http://contacts.gaiamobile.org,http://dialer.gaiamobile.org,http://dialer.gaiamobile.org/background.html,http://dialer.gaiamobile.org/oncall.html");
-user_pref("dom.mozBluetooth.whitelist","http://system.gaiamobile.org,http://settings.gaiamobile.org");
-user_pref("dom.mozBrowserFramesWhitelist","http://homescreen.gaiamobile.org,http://browser.gaiamobile.org,http://system.gaiamobile.org");
-user_pref("dom.mozApps.whitelist","http://homescreen.gaiamobile.org,http://system.gaiamobile.org,http://dialer.gaiamobile.org,http://dialer.gaiamobile.org/oncall.html");
-user_pref("dom.mobileconnection.whitelist","http://system.gaiamobile.org,http://dialer.gaiamobile.org,http://dialer.gaiamobile.org/oncall.html,http://settings.gaiamobile.org");
-user_pref("dom.mozFMRadio.whitelist","http://fm.gaiamobile.org");
-user_pref("dom.systemXHR.whitelist","http://browser.gaiamobile.org");
-user_pref("dom.allow_scripts_to_close_windows", true);
+user_pref("dom.power.whitelist","app://system.gaiamobile.org,app://dialer.gaiamobile.org,app://test-agent.gaiamobile.org");
+user_pref("dom.sms.whitelist","app://sms.gaiamobile.org");
+user_pref("dom.mozContacts.whitelist","app://contacts.gaiamobile.org,app://sms.gaiamobile.org,app://dialer.gaiamobile.org");
+user_pref("dom.telephony.app.phone.url","app://system.gaiamobile.org,app://system.gaiamobile.org/index.html,app://dialer.gaiamobile.org,app://dialer.gaiamobile.org/index.html,app://dialer.gaiamobile.org/background.html,app://dialer.gaiamobile.org/oncall.html");
+user_pref("dom.mozBluetooth.whitelist","app://system.gaiamobile.org,app://settings.gaiamobile.org");
+user_pref("dom.voicemail.whitelist","app://system.gaiamobile.org,app://dialer.gaiamobile.org");
+user_pref("dom.mozBrowserFramesWhitelist","app://homescreen.gaiamobile.org,app://browser.gaiamobile.org,app://system.gaiamobile.org");
+user_pref("dom.mozApps.whitelist","app://homescreen.gaiamobile.org,app://system.gaiamobile.org,app://dialer.gaiamobile.org");
+user_pref("dom.mobileconnection.whitelist","app://system.gaiamobile.org,app://dialer.gaiamobile.org,app://settings.gaiamobile.org");
+user_pref("dom.mozFMRadio.whitelist","app://fm.gaiamobile.org");
+user_pref("dom.systemXHR.whitelist","app://calendar.gaiamobile.org,app://browser.gaiamobile.org,app://email.gaiamobile.org");
